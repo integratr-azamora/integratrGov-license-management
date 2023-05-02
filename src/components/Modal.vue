821bbba4c1d5d511ec7999553bcc902bdd56530a
Vanessa rose Consigna <vconsigna.intern.integratr@outlook.com>
2023-05-02 18:16:05 +0800

    fixed modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,30 +1,32 @@
 <template>
     <!-- con1 -->
     <br>
-    <div class="container bor-style-solid perma con1" >
+    <div class="container bor-style-solid-perma con1" >
        <div class="modal-header">
           <hr>
-          <button class="component-button-round-indigo-none-indigo-nofont-fontsize24px-white-mx10-my-10  ">
+          <div class="position-absolute top-0 end-0">
+          <button class="component-button-round-indigo-none-indigo-nofont-fontsize24px-white-mx10-my-10">
              <div class="rotate-45"><i class="isax isax-add "></i></div>
           </button>
+          </div>
        </div>
        <div id="info" class="text-center">
           <img src="../assets/icon/info.svg" class="center info2" alt="info">
        </div>
        <div class="text-center">
           <div class="modal-body">
-             <p class="font-style-roboto">Do you want to submit?</p>
-             <p class="font-style-gotham">You can always go back and update if there are any changes.</p>
+             <p class="font-style-roboto mt-1">Do you want to submit?</p>
+             <p class="font-style-gotham mt-1">You can always go back and update if there are any changes.</p>
           </div>
        </div>
-       <div class="text-center">
-          <button class="component-button-none-solid-indigo-roboto-16px-indigo-px10-py20-mx10-my-10 mt-3">Cancel</button>
-          <button class="component-button-indigo-solid-indigo-roboto-16px-white-px10-py20-mx10-my-10 mt-3">Submit</button>
+       <div class="text-center mt-3">
+          <button class="component-button-none-solid-indigo-roboto-16px-indigo-px10-py20-mx10-my-10">Cancel</button>
+          <button class="component-button-indigo-solid-indigo-roboto-16px-white-px10-py20-mx10-my-10">Submit</button>
        </div>
     </div>
     <!-- con2 -->
     <br>
-    <div class="container bor-style-solid perma con2" >
+    <div class="container bor-style-solid-perma con2" >
        <div class="modal-header">
           <p class="modal-header1 m-1"><b>Tag User</b></p>
           <button class="component-button-round-indigo-none-indigo-nofont-fontsize24px-white-mx10-my-10  ">
@@ -46,7 +48,7 @@
     </div>
     <!-- con3 -->
     <br>
-    <div class="container bor-style-solid perma con3" >
+    <div class="container bor-style-solid-perma con3" >
        <div class="modal-header">
           <p class="modal-header1 m-1"><b>Transfer Ownership</b></p>
           <button class="component-button-round-indigo-none-indigo-nofont-fontsize24px-white-mx10-my-10  ">
@@ -63,7 +65,7 @@
     </div>
     <!-- con4 -->
     <br>
-    <div class="container bor-style-solid perma con4" >
+    <div class="container bor-style-solid-perma con4" >
        <div class="modal-header">
           <p class="modal-header1"><b>Existing Business</b></p>
           <button class="component-button-round-indigo-none-indigo-nofont-fontsize24px-white-mx10-my-10  ">
@@ -77,8 +79,10 @@
           <input class="form-control-1 mt-2" type="text" placeholder="License No." aria-label="Disabled input example" disabled>
           <input class="form-control-1 mt-2" type="text" placeholder="TIN No.." aria-label="Disabled input example" disabled>
           <input class="form-control-1 mt-2" type="text" placeholder="Date Established" aria-label="Disabled input example" disabled>
+          <i class="icon-size m-2 gray isax isax-calendar-1" style="position:relative;left:420px; bottom: 40px;"></i>
+
           <div class="modal-footer">
-             <button class="component-button-indigo-solid-indigo-roboto-16px-white-px20-py20-mx20-my-10 mt-3">Search</button>
+             <button class="component-button-indigo-solid-indigo-roboto-16px-white-px20-py20-mx20-my-10">Search</button>
           </div>
        </div>
     </div>
