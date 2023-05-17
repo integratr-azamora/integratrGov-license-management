16efb5f32c4ef8e7f4c7e3ab635c161dfbb9de65
Melanio III Flores <128350843+integratr-intern-mflores@users.noreply.github.com>
2023-05-17 17:10:44 +0800

    sprint 3

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import AccountManager from '../components/AccountManager.vue';
 import LicenseManager from '../components/LicenseManger.vue';
 import LicensekeyIssue from '../components/Licensekey-Issue.vue';
 
+import LicenseKeyAssigned from '../components/Sprint3/LicenseManagerAssigned.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -81,6 +82,15 @@ const router = createRouter({
       name: 'addaccount-clientdetails',
       component: AddAccountClientDetails
     }
+    // sprint 3 
+    ,
+    {
+      path: '/licensekey-assigned',
+      name: 'licensekey-assigned',
+      component: LicenseKeyAssigned
+    }
+
+    // sprint 3
     
   ]
 })
