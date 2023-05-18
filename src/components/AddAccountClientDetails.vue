d922bcfd866501fc3c18bd4b23f3806081403cd0
integratr-intern-vanessa <128349685+integratr-intern-vanessa@users.noreply.github.com>
2023-05-18 17:57:31 +0800

    updated

--- src/components/AddAccountClientDetails.vue
+++ src/components/AddAccountClientDetails.vue
@@ -1,24 +1,24 @@
 <template>
-   <div class="container bor-style-solid-perma client-cont1" >
-      <span class="client-roboto-fonts mt-10px p-3">Juan Dela Cruz</span>
+   <div class="client-cont1" >
+      <span class="client-roboto-fonts mt-20px">Juan Dela Cruz</span>
       <div class="row">
          <!-- container-left  -->
-         <div class="col client-container mt-20px">
+         <div class="col client-container mt-20px mx-10">
             <div class="d-flex justify-content-between">
-               <span class="font-style-inter-semibold p-3">Client Information</span>
-               <button class="button-default bg-none color-purpleheart border-color-purpleheart">Edit</button>
+               <span class="font-style-inter-semibold">Client Information</span>
+               <button class="button-default bg-none color-purpleheart border-color-purpleheart m-20px">Edit</button>
             </div>
             <div class="scroll client-details-table-height" >
                <table class="table table-striped-custom table-hover font-table-text table-borderless">
-                  <thead >
+                  <thead class="table-header">
                      <tr >
-                        <th class="text-start p-20px">
+                        <th class="text-start">
                            <div class="d-flex align-items-center">
                               <span class="font-table-column">Label</span>
                               <i class="isax isax-arrow-down5 size-icon-table-arrow"></i>
                            </div>
                         </th>
-                        <th class="text-start px-20px">
+                        <th class="text-start">
                            <div class="d-flex align-items-center">
                               <span class="font-table-column">Details</span>
                               <i class="isax isax-arrow-down5 size-icon-table-arrow"></i>
@@ -27,59 +27,59 @@
                      </tr>
                   </thead>
                   <tr class="size-table-tabledata column">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">First Name</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">Juan</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">First Name</td>
+                     <td class="text-start vertical-alignment-center table-text-light">Juan</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Last Name</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">Dela Cruz</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Last Name</td>
+                     <td class="text-start vertical-alignment-center table-text-light">Dela Cruz</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Middle Name</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">Santos</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Middle Name</td>
+                     <td class="text-start vertical-alignment-center table-text-light">Santos</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Email</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">juandelacruz@gmail.com</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Email</td>
+                     <td class="text-start vertical-alignment-center table-text-light">juandelacruz@gmail.com</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Mobile No.</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">+639091234567</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Mobile No.</td>
+                     <td class="text-start vertical-alignment-center table-text-light">+639091234567</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">TIN</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">00000000000</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">TIN</td>
+                     <td class="text-start vertical-alignment-center table-text-light">00000000000</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">LGU/Organization</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">Pasig City</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">LGU/Organization</td>
+                     <td class="text-start vertical-alignment-center table-text-light">Pasig City</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Position</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">Mayor</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Position</td>
+                     <td class="text-start vertical-alignment-center table-text-light">Mayor</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Unit/House/Bldg No.</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">1</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Unit/House/Bldg No.</td>
+                     <td class="text-start vertical-alignment-center table-text-light">1</td>
                   </tr>
                   <tr class="size-table-tabledata column border-rouded-5px">
-                     <td class="text-start px-20px vertical-alignment-center table-text-roboto-reg">Building Name</td>
-                     <td class="text-start px-20px vertical-alignment-center table-text-light">SM City Light Mall Residence</td>
+                     <td class="text-start vertical-alignment-center table-text-roboto-reg">Building Name</td>
+                     <td class="text-start vertical-alignment-center table-text-light">SM City Light Mall Residence</td>
                   </tr>
                </table>
             </div>
          </div>
          <!-- container-left  -->
          <!-- container-right  -->
-         <div class="action-cont4 mt-20px">
-            <div class=" border-color-frenchskyblue">
+         <div class="action-cont4 mt-20px mx-20px">
+            <div class="border-bottom-license mt-20px">
                <div class="d-flex flex-row align-items-center">
                   <div class="col-auto">
                      <span class="fontfamily-gotham-heading-2 blue">Actions</span>
                   </div>
-                  <div class="col d-flex justify-content-end">
-                     <button class="button-default bg-valencia color-white border-color-valencia d-flex align-items-center"><i class="isax isax-minus icon-size-weight me-1"></i>Reset Password</button>
-                     <button class="button-default bg-purpleheart color-white border-color-purpleheart d-flex align-items-center"><i class="isax isax-trash icon-size-weight me-1"></i>Delete</button>
+                  <div class="col button-container d-flex justify-content-end me-10px">
+                     <button class="button-default bg-valencia color-white border-color-valencia d-flex align-items-center"><i class="isax isax-minus icon-size-weight"></i>Reset Password</button>
+                     <button class="button-default bg-purpleheart color-white border-color-purpleheart d-flex align-items-center"><i class="isax isax-trash icon-size-weight"></i>Delete</button>
                   </div>
                </div>
             </div>
@@ -88,7 +88,7 @@
                <textarea style="resize:none" class="border-color-none"></textarea>
             </div>
             <div class="d-flex justify-content-end">
-               <button class="button-default bg-none color-purpleheart border-color-purpleheart mt-30px">Edit</button>
+               <button class="button-default bg-none color-purpleheart border-color-purpleheart mt-20px me-10px">Edit</button>
             </div>
          </div>
          <!-- container-right  -->
