8f47338bab27e264c5b1577ef023c976bdf5b6b9
Melanio III Flores <128350843+integratr-intern-mflores@users.noreply.github.com>
2023-05-19 08:45:52 +0800

    License Management

--- src/components/Sprint3/SoftwareLicenseManagement.vue
+++ src/components/Sprint3/SoftwareLicenseManagement.vue
@@ -5,8 +5,8 @@
              <div class=" heading1 mb-20px mt-20px">License Manager </div>
              <div>
                 <button class="button-tab"><a class="dropdown-item " href="/licmanager">Unassigned License Keys</a></button>
-                <button class="button-tab">Assigned License Keys</button>
-                <button class="button-tab-active">Software License Management</button>
+                <button class="button-tab"><a class="dropdown-item " href="/licensekey-assigned">Assigned License Keys</a></button>
+                <button class="button-tab-active"><a class="dropdown-item " href="/software-license-management">Software License Management</a></button>
              </div>
              <!-- row1  -->
              <div class=" shadow-sm border-round16px pb-20px pt-10px">
