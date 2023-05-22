f9cf185c7538684017e243fc8fa2e06d5c5b4c23
integratr-intern-vanessa <128349685+integratr-intern-vanessa@users.noreply.github.com>
2023-05-22 11:34:04 +0800

    Vane PR

--- src/components/Sprint3/TransferLicenseModal.vue
+++ src/components/Sprint3/TransferLicenseModal.vue
@@ -3,13 +3,13 @@
       <div class="transfer-modal-cont">
          <div class="d-flex justify-content-between my-25px mx-20px">
             <p class="license-header">Transfer License</p>
-            <button class="mx-20px button-round bg-indigo color-white border-color-indigo d-flex align-items-center">
+            <button class="button-round bg-indigo color-white border-color-indigo d-flex align-items-center">
                <fa class="font-size-20px" icon="close" />
             </button>
          </div>
          <div class="border-bottom-license mt-15px"></div>
          <div class="row mt-20">
-            <div class="col ms-20px me-20px">
+            <div class="col ms-20px mx-20px">
                <div class="row mt-20px">
                   <div class="row d-flex align-items-center">
                      <span class=" d-flex justify-content-start">
@@ -33,11 +33,11 @@
             </div>
          </div>
          <!-- 1st -->
-         <form class="form-floating m-20px mt-20px">
+         <form class="form-floating mx-20px my-20px">
             <input type="text" class="form-control license-form Roboto-input-box-font border-color-none" id="information" value="sebastiancruz@gmail.com" >
             <label for="information" class="Roboto-label-font">Transfer License to this Account (Email)</label>
          </form>
-         <div class="d-flex text-center justify-content-end mx-20px my-20px">
+         <div class="d-flex text-center justify-content-end mx-20px my-33px">
             <button class="button-default bg-none border-color-none color-indigo me-2">Cancel</button>
             <button class="button-default bg-indigo border-color-indigo color-white me-0">Save</button>
          </div>
