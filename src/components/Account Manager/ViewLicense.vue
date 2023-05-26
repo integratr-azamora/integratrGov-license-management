a1e8031b3274170405c6987729a8fa1cd62c3511
integratr-intern-vanessa <128349685+integratr-intern-vanessa@users.noreply.github.com>
2023-05-26 09:09:14 +0800

    vane

--- src/components/Account Manager/ViewLicense.vue
+++ src/components/Account Manager/ViewLicense.vue
@@ -1,21 +1,3 @@
 <template>
-<div class="d-flex justify-content-center">
-    <div class="view-license">
-        <div class=" heading1 padding">License Details</div>
-        <div class="view-license-tab1">
-            <div class="container text-center">
-  <div class="row">
-    <div class="col-md-4">.col-md-4</div>
-    <div class="col-md-4 offset-md-4">.col-md-4 .offset-md-4</div>
-  </div>
-  </div>
-
-
-
-        </div>
-    </div>
-
-</div>
-    
     
 </template>
