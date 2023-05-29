40d739a39d5685578f3ad6573054176b3bd930ff
integratr-intern-vanessa <128349685+integratr-intern-vanessa@users.noreply.github.com>
2023-05-30 00:22:36 +0800

    Update

--- src/components/Account Manager/ViewLicense.vue
+++ src/components/Account Manager/ViewLicense.vue
@@ -1,83 +1,159 @@
 <template>
-    <div class="container">
-       <div class="">
-          <div class=" heading1 padding">License Details</div>
-          <div class="view-license-tab1">
-             <div class="row border-color-indigo ">
-               <div class="col-12 col-lg-6  border-color-eucalyptus p-0">
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">Client</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">Juan Dela Cruz</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+   <div class="container">
+      <div class="">
+         <div class=" heading1 padding">License Details</div>
+         <div class="view-license-tab1 py-20px mt-20px px-0">
+            <div>
+               <div class="row">
+               <div class="col-12 col-lg-6  p-0">
+                  <div class="row ms-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Client</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">Juan Dela Cruz</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">TIN:</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">00000000000</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">TIN:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">00000000000</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">LGU/Organization:</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">Pasig City</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">LGU/Organization:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">Pasig City</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">Date Assigned</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">01/01/2021</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Date Assigned</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">01/01/2021</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-                  
                </div>
-               <div class="col-12 col-lg-6 border-color-valencia p-0">
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">Org Account ID:</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">GOV112223300</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+               <div class="col-12 col-lg-6  p-0">
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Org Account ID:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">GOV112223300</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">Email Address:</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">juandelacruz@gmail.com</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Email Address:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">juandelacruz@gmail.com</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-                  <div class="row">
-                     <span class="col-4 border-color-purpleheart" style=" white-space: nowrap;">Contact No:</span>
-                     <span class="col-4 border-color-frenchskyblue" style=" white-space: nowrap;">+63 9091234567</span>
-                     <span class="col-4 border-color-cornflowerblue" style=" white-space: nowrap;"></span>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Contact No:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">+63 9091234567</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
                   </div>
-
                </div>
-             </div>
-          </div>
-       </div>
-    </div>
-
-    <div class="container">
-       <div>
-          <div class="view-license-tab1">
-            <div class="row">
-               <div class="col-12 col-sm-6 col-md-6 col-lg-6 p-0">
-               <form class="form-floating">
-                  <input type="email" class="form-control bg-whitesmoke" id="floatingInputValue" placeholder="name@example.com" value="test@example.com">
-                  <label for="floatingInputValue">Input with value</label>
-               </form>
             </div>
-            <div class="col-12 col-sm-6 col-md-6 col-lg-6 p-0">
-               <form class="form-floating">
-                  <input type="email" class="form-control bg-whitesmoke" id="floatingInputValue" placeholder="name@example.com" value="test@example.com">
-                  <label for="floatingInputValue">Input with value</label>
-               </form>
-            </div>
-            <div class="col-12 p-0">
-               <form class="form-floating">
-                  <input type="email" class="form-control bg-whitesmoke" id="floatingInputValue" placeholder="name@example.com" value="test@example.com">
-                  <label for="floatingInputValue">Input with value</label>
-               </form>
-            </div>
-            
 
             </div>
             
-          </div>
          </div>
       </div>
-
- </template>
+      <div class="">
+         <div class="view-license-tab1 py-20px mt-20px px-0">
+            <div>
+               <div class="row">
+               <div class="col-12 col-lg-6  p-0">
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Activation Code: </span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">FSPDE-00000-00000-00000-00000</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">License Description:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">Perpetual</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Product No.:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">A01</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Program/Part No.:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">A1AA1AA</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Validity</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">40 years</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+               </div>
+               <div class="col-12 col-lg-6  p-0">
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Module: </span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">Business Permit and License</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Version:</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">V.1.0</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Platform</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">SaaS</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Grace Period</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;">7 days</span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+                  <div class="row ms-20px mt-20px">
+                     <span class="col-4 view-record-font-heading3" style=" white-space: nowrap;">Expiry Date</span>
+                     <span class="col-4 font-style-roboto-p1" style=" white-space: nowrap;"><span>00/00/0000</span> <span class="ms-3 font-style-roboto-view">Not yet activated</span> </span>
+                     <span class="col-4 " style=" white-space: nowrap;"></span>
+                  </div>
+               </div>
+            </div>
+            </div>
+            <div class="d-flex flex-wrap flex-row  align-middle ms-5px mt-20px">
+                  <button class="button-default m-10px bg-none color-indigo border-color-indigo">Update License</button>
+                  <button class="button-default m-10px bg-none color-indigo border-color-indigo">Transfer License</button>
+                  <button class="button-default m-10px bg-none color-dimgray border-color-dimgray">Renew License</button>
+                  <button class="button-default m-10px bg-valencia color-white border-color-valencia d-flex flex-row"><i class="isax isax-minus icon-size-weight me-1"></i>Suspend</button>
+                  <button class="button-default m-10px bg-indigo color-white border-color-indigo d-flex flex-row">
+             <i class="isax isax-trash icon-size-weight me-1"></i>
+             Remove
+          </button>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="container mt-20px">
+      <div>
+         <div class="view-license-tab1">
+            <div class="row">
+               <div class="col-12 col-sm-6 col-md-6 col-lg-6">
+                  <form class="form-floating">
+                     <input type="email" class="form-control bg-whitesmoke border-color-none mb-10px" id="floatingInputValue"
+                        placeholder="name@example.com" value="Sebastian Cruz">
+                     <label for="floatingInputValue">Last Updated by</label>
+                  </form>
+               </div>
+               <div class="col-12 col-sm-6 col-md-6 col-lg-6">
+                  <form class="form-floating">
+                     <input type="email" class="form-control bg-whitesmoke border-color-none mb-10px" id="floatingInputValue"
+                        placeholder="name@example.com" value="01/01/2022 9:23:00 AM">
+                     <label for="floatingInputValue">Date Updated</label>
+                  </form>
+               </div>
+               <div class="col-12 p-0">
+                  <div class="form-floating">
+            <div class=" mt-10px mx-10px">
+               <div class="form-floating">
+                  <textarea class="form-control size-textarea-view border-color-none bg-whitesmoke" placeholder="Leave a comment here" id="floatingTextarea2">
+License extension: transfer of remaining months from old subscription                  </textarea>
+                  <label for="information">Remarks</label>
+               </div>
+            </div>
+         </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
